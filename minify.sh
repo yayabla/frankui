4524606db5cfa3b2e3f4e9697b6a63d0fcef2cbc
Frank iT-APi <frank@it-api.nl>
2026-07-20 11:17:36 +0200

    Changed minify.sh and removed local paths

--- minify.sh
+++ minify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directories
-FRAMEWORK_DIR="/home/frank/Bureaublad/bscss/frankui/framework"
+FRAMEWORK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Temporary bundle files
 TEMP_JS="$FRAMEWORK_DIR/temp_bundle.js"
@@ -77,56 +77,4 @@ else
 fi
 echo "✔ Created CSS bundle at: $FRAMEWORK_DIR/frankui.bundle.min.css"
 
-# Copy files to target directories
-echo "=== Copying files to target directories ==="
-
-# 1. npm/
-NPM_DIR="/home/frank/Bureaublad/bscss/frankui/npm"
-if [ -d "$NPM_DIR" ]; then
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.js" "$NPM_DIR/frankui.bundle.min.js"
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.css" "$NPM_DIR/frankui.bundle.min.css"
-    # Copy README if it exists in git repo
-    if [ -f "/home/frank/Bureaublad/bscss/frankui/production/git-repo/frankui/README.md" ]; then
-        cp "/home/frank/Bureaublad/bscss/frankui/production/git-repo/frankui/README.md" "$NPM_DIR/README.md"
-    fi
-    # Copy LICENSE if it exists in git repo
-    if [ -f "/home/frank/Bureaublad/bscss/frankui/production/git-repo/frankui/LICENSE" ]; then
-        cp "/home/frank/Bureaublad/bscss/frankui/production/git-repo/frankui/LICENSE" "$NPM_DIR/LICENSE"
-    fi
-    echo "✔ Copied bundle files to: $NPM_DIR"
-fi
-
-# 2. production/www/
-PROD_WWW_JS="/home/frank/Bureaublad/bscss/frankui/production/www/js"
-PROD_WWW_CSS="/home/frank/Bureaublad/bscss/frankui/production/www/css"
-if [ -d "$PROD_WWW_JS" ]; then
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.js" "$PROD_WWW_JS/frankui.bundle.min.js"
-    echo "✔ Copied JS bundle to: $PROD_WWW_JS"
-fi
-if [ -d "$PROD_WWW_CSS" ]; then
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.css" "$PROD_WWW_CSS/frankui.bundle.min.css"
-    echo "✔ Copied CSS bundle to: $PROD_WWW_CSS"
-fi
-
-# 3. production/plugins/framework/
-PROD_PLUGINS_FW="/home/frank/Bureaublad/bscss/frankui/production/plugins/framework"
-if [ -d "$PROD_PLUGINS_FW" ]; then
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.js" "$PROD_PLUGINS_FW/frankui.bundle.min.js"
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.css" "$PROD_PLUGINS_FW/frankui.bundle.min.css"
-    echo "✔ Copied bundle files to: $PROD_PLUGINS_FW"
-fi
-
-# 4. production/git-repo/frankui/www/
-GIT_REPO_WWW_JS="/home/frank/Bureaublad/bscss/frankui/production/git-repo/frankui/www/js"
-GIT_REPO_WWW_CSS="/home/frank/Bureaublad/bscss/frankui/production/git-repo/frankui/www/css"
-if [ -d "$GIT_REPO_WWW_JS" ]; then
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.js" "$GIT_REPO_WWW_JS/frankui.bundle.min.js"
-    echo "✔ Copied JS bundle to: $GIT_REPO_WWW_JS"
-fi
-if [ -d "$GIT_REPO_WWW_CSS" ]; then
-    cp "$FRAMEWORK_DIR/frankui.bundle.min.css" "$GIT_REPO_WWW_CSS/frankui.bundle.min.css"
-    echo "✔ Copied CSS bundle to: $GIT_REPO_WWW_CSS"
-fi
-
 echo "=== Minification Done! ==="
-
